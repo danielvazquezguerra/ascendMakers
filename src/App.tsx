4a835fde0d5cfeceabfefbe1c18c42154ff14200
danielvazquezguerra <daniel.vazquezquerra@gmail.com>
2024-04-22 19:43:49 +0200

    instalando talwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import classNames from 'classnames';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App container">
+    <div style={{border: '1px solid black'}} className={classNames('flex', 'flex-col', 'h-screen')}>
       <h1 className='text-center'>Ascend Test App</h1>
     </div>
   );
