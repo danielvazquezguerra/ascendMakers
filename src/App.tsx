c54ec02c5f4e37b39638491566d5844a08c90a74
danielvazquezguerra <daniel.vazquezquerra@gmail.com>
2024-04-22 19:18:37 +0200

    subiendo proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1 className='text-center'>Ascend Test App</h1>
+    </div>
+  );
+}
+
+export default App;
